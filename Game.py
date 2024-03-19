e2626c59ceab8a26982def24dc3087984e5247ab
varunsin1 <95243683+varunsin1@users.noreply.github.com>
2024-03-18 18:39:24 -0700

    Update Game.py

--- Game.py
+++ Game.py
@@ -19,7 +19,7 @@ def value(turn):
                 value += 1
             else:
                 value += 11
-return value
+    return value
 #dealing out the deck
 def deal(turn):
     card = random.choice(cards)
@@ -37,8 +37,7 @@ def showdealercards():
          return dealercards[0], dealercards[1]
 #stand or hit for the player
 while playerin or dealerin:
-    print(f"The dealer has {value(dealercards)} from {showdealercards()}
-and ?")
+    print(f"The dealer has {value(dealercards)} from {showdealercards()} and ?")
     print(f"You have {value(playercards)} from {playercards}")
     if playerin:
         standorhit = input("Do you want to stand or hit?\n")
@@ -54,35 +53,27 @@ and ?")
     if value(playercards) >= 21:
         break
     elif value(dealercards) >= 21:
-break
+        break
 #how to know if the dealer or player has won
 if value(playercards) == 21:
-    print(f"You have {playercards} for {value(playercards)} and the dealer
-has {dealercards} for {value(dealercards)}")
+    print(f"You have {playercards} for {value(playercards)} and the dealer has {dealercards} for {value(dealercards)}")
     print("YOU WIN!")
 elif value(dealercards) == 21:
-    print(f"You have {playercards} for {value(playercards)} and the dealer
-has {dealercards} for {value(dealercards)}")
+    print(f"You have {playercards} for {value(playercards)} and the dealer has {dealercards} for {value(dealercards)}")
     print("THE DEALER WINS!")
 elif value(playercards) > 21:
-    print(f"You have {playercards} for {value(playercards)} and the dealer
-has {dealercards} for {value(dealercards)}")
+    print(f"You have {playercards} for {value(playercards)} and the dealer has {dealercards} for {value(dealercards)}")
     print("THE DEALER WINS, YOU HAVE BUST!")
 elif value(dealercards) > 21:
-    print(f"You have {playercards} for {value(playercards)} and the dealer
-has {dealercards} for {value(dealercards)}")
- 
-     print("YOU WIN, THE DEALER HAS BUST!")
+    print(f"You have {playercards} for {value(playercards)} and the dealer has {dealercards} for {value(dealercards)}")
+    print("YOU WIN, THE DEALER HAS BUST!")
 elif 21 - value(dealercards) < 21 - value(playercards):
-    print(f"You have {playercards} for {value(playercards)} and the dealer
-has {dealercards} for {value(dealercards)}")
+    print(f"You have {playercards} for {value(playercards)} and the dealer has {dealercards} for {value(dealercards)}")
     print("YOU WIN!")
 elif 21 - value(dealercards) > 21 - value(playercards):
-    print(f"You have {playercards} for {value(playercards)} and the dealer
-has {dealercards} for {value(dealercards)}")
+    print(f"You have {playercards} for {value(playercards)} and the dealer has {dealercards} for {value(dealercards)}")
     print("THE DEALER WINS!")
 elif 21 - value(dealercards) == 21 - value(playercards):
-    print(f"You have {playercards} for {value(playercards)} and the dealer
-has {dealercards} for {value(dealercards)}")
+    print(f"You have {playercards} for {value(playercards)} and the dealer has {dealercards} for {value(dealercards)}")
     print("YOU ARE TIED WITH THE DEALER!")
  
